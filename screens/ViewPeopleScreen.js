5752ab7a522780e3297321a476995ad9899d7af3
Filipe100 <filipe.thornhill@studytafensw.edu.au>
2023-11-27 13:01:24 +1100

    changes make added screens

--- screens/ViewPeopleScreen.js
+++ screens/ViewPeopleScreen.js
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import { View, ScrollView } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { infoMessage} from '../utils/flashMessage';
+import NetInfo from '@react-native-community/netinfo';
 
 // Import helper code
 import Settings from '../constants/Settings';
-import { RoiGetPeople } from '../utils/RoiApi';
-import { PopupOk } from "../utils/Popup";
+import { RoiDeletePerson, RoiGetPeople } from '../utils/RoiApi';
+import { PopupOk, PopupOkCancel } from "../utils/Popup";
 
 // Import styling and components
-import { TextParagraph, TextH1, TextH2 } from "../components/StyledText";
+import { TextParagraph, TextH1, TextH2, Text} from "../components/StyledText";
 import Styles from "../styles/MainStyle";
 import { MyButton } from '../components/MyButton';
-
+import { ButtonContainer } from '../components/ButtonContainer';
 
 export default function ViewPeopleScreen(props) {
 
@@ -44,11 +46,113 @@ export default function ViewPeopleScreen(props) {
   }
 
   function showAddPerson() {
-    
-    console.log("show add person...")
-
+    // Navigate to AddPerson and replace the current screen
+    props.navigation.replace('Root', { screen: 'AddPerson' });
   }
 
+  function showViewPerson(person) {
+    // Navigate to ViewPerson and pass through the person's ID as a param
+    props.navigation.navigate('ViewPerson', { id: person.id });
+  }
+
+  function showEditPerson(person) {
+    // Navigate to EditPerson and pass through the person's ID as a param
+    props.navigation.navigate('EditPerson', { id: person.id });
+  }
+
+  /**
+   * Delete a person from the database
+   * @param {Person} person The person to delete.
+   */
+  function deletePerson(person) {
+    // Check if person should be deleted (confirm with user)
+    PopupOkCancel(
+      // Title and message
+      'Delete person?',
+      `Are you sure you want to delete ${person.name}`,
+
+      // 0K - delete the person
+      () => {
+        // Delete the person using the API
+        RoiDeletePerson(person.id)
+          .then((data) => {
+            // Show confirmation that the person has been deleted
+            PopupOk('Person deleted', `${person.name} has been  deleted`);
+            // Refresh the person list
+            refreshPersonList();
+          })
+          .catch((error) => {
+            // Display error
+            PopupOk('API Error', 'Could not delete person');
+          });
+        // console.log('Ok.. deleting person');
+      },
+      // Cancel do nothing
+      () => {
+        console.log('Cancel - no delete for you!');
+      }
+    );
+  }
+
+  // Display flash message banner if offline
+  function displayConnectionMessage() {
+    console.log('displayConnectionMessage');
+    // Get network connection status
+    NetInfo.fetch().then((status) => {
+      // Check if not connected to the Internet
+      if (!status.isConnected) {
+        // Display the flash message
+        infoMessage('No internet connection', 'You will only see cached data until you \nhave an active internet connection again');
+      }
+    });
+  }
+
+  /**
+   * Delete a person from the database
+   * @param {Person} person The person to delete.
+   */
+  function deletePerson(person) {
+    // Check if person should be deleted (confirm with user)
+    PopupOkCancel(
+      // Title and message
+      'Delete person?',
+      `Are you sure you want to delete ${person.name}`,
+
+      // 0K - delete the person
+      () => {
+        // Delete the person using the API
+        RoiDeletePerson(person.id)
+          .then((data) => {
+            // Show confirmation that the person has been deleted
+            PopupOk('Person deleted', `${person.name} has been  deleted`);
+            // Refresh the person list
+            refreshPersonList();
+          })
+          .catch((error) => {
+            // Display error
+            PopupOk('API Error', 'Could not delete person');
+          });
+        // console.log('Ok.. deleting person');
+      },
+      // Cancel do nothing
+      () => {
+        console.log('Cancel - no delete for you!');
+      }
+    );
+  }
+
+  // Display flash message banner if offline
+  function displayConnectionMessage() {
+    console.log('displayConnectionMessage');
+    // Get network connection status
+    NetInfo.fetch().then((status) => {
+      // Check if not connected to the Internet
+      if (!status.isConnected) {
+        // Display the flash message
+        infoMessage('No internet connection', 'You will only see cached data until you \nhave an active internet connection again');
+      }
+    });
+  }
 
   // Display all people data
   function displayPeople() {
@@ -58,8 +162,35 @@ export default function ViewPeopleScreen(props) {
 
       // Create an output view for each item
       return (
-        <View key={p.id}>
+        <View key={p.id} style={Styles.dataContainerHorizontal}>
+          <View style={Styles.personListItemDetails}>
           <TextParagraph>{p.name}</TextParagraph>
+          <TextParagraph>{p.department?.name ?? '---'}</TextParagraph>
+          <TextParagraph>{p.phone}</TextParagraph>
+          </View>
+          <ButtonContainer direction="column">
+            <MyButton 
+            text="Info"
+            type="major"
+            size="small"
+            buttonStyle={Styles.personListItemButton}
+            textStyle={Styles.personListItemButtomText}
+            />
+            <MyButton          
+            text="Edit"
+            type="default"
+            size="small" 
+            buttonStyle={Styles.personItemListButton}
+            textStyle={Styles.personListItemButtomText}
+            />
+            <MyButton          
+            text="Delete"
+            type="minor"
+            size="small" 
+            buttonStyle={Styles.personListItemButton}
+            textStyle={Styles.personListItemButtomText}
+            />
+          </ButtonContainer>
         </View>
       )
 
